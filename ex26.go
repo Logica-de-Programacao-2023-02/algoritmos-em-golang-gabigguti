de5c503e07f7770bee50eeb03b7e34d1997bdbe0
gabigguti <119536472+gabigguti@users.noreply.github.com>
2023-09-03 17:01:36 -0300

    Update ex26.go

--- ex26.go
+++ ex26.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var num int
+	fmt.Print("Informe um numero: ")
+	fmt.Scan(&num)
+
+	var mult int = 1
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(num * mult)
+		mult++
+
+	}
 }
